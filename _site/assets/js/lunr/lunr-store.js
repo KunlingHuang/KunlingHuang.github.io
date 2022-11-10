13e6f6b025fdb10eb41281af558d31cf927ecce7
KunlingHuang <khuang82@wisc.edu>
2022-11-09 21:03:01 -0600

    2022 update

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,12 +2,12 @@ var store = [{
         "title": "Academic statement writing",
         "excerpt":"A list of improving your academic statement writing.  ","categories": [],
         "tags": ["Advice"],
-        "url": "/2020/10/10/Academic-statement-writing/",
+        "url": "http://localhost:4000/2020/10/10/Academic-statement-writing/",
         "teaser": null
       },{
         "title": "Pipeline documentation template",
         "excerpt":"The README template I use. ######################### # Project # ######################### ===== Dates ===== Dates of major updates and description of the updates ==== INFO ==== Input: description of inputs, including the path and format Output: description of outputs, including the path and format Summary: a brief summary of the pipeline...","categories": [],
         "tags": ["Advice"],
-        "url": "/2020/10/16/Pipeline-documentation-template/",
+        "url": "http://localhost:4000/2020/10/16/Pipeline-documentation-template/",
         "teaser": null
       }]
